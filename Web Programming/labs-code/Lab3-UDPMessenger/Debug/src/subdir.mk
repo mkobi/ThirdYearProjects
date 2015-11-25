1a1ec3b44aa80338f91590b18c4eb144e520de03
Kobi Mizrahi <kobi.mizrahi@gmail.com>
2015-11-25 19:06:51 +0200

    Web Programming course initial commit.

---
+++ Web Programming/labs-code/Lab3-UDPMessenger/Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/MThread.cpp \
+../src/UDPMessenger.cpp \
+../src/UDPSocket.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/MThread.o \
+./src/UDPMessenger.o \
+./src/UDPSocket.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/MThread.d \
+./src/UDPMessenger.d \
+./src/UDPSocket.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
